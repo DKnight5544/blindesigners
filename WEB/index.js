dc4e92aa013daab59666ff3e00f95108bab213a6
David Knight <dave@mostlyspam>
2024-06-25 23:18:18 -0700

    More textual changes.

--- WEB/index.js
+++ WEB/index.js
@@ -5,11 +5,12 @@ function begin() {
     let id = getUrlParam("id");
 
     if (id === null) {
-        id = "ScanMe_0001";
+        id = "DWK_ScanMe";
     } 
 
     const image = document.getElementById("QRCodeImage");
-    const sampleName = id.split("_")[0];
+    const sampleName = id.split("_")[1];
+    id = id.split("_")[0];
     image.src = `samples/${sampleName}.png`;
 
     const htmlBody = document.body.innerHTML;
