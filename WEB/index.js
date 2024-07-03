c19d34ab3f65ca4399fdd6f26c58a26196e067b4
David Knight <dave@mostlyspam>
2024-07-02 21:46:07 -0700

    ID does not include mobster

--- WEB/index.js
+++ WEB/index.js
@@ -14,7 +14,6 @@ function begin() {
 
     const image = document.getElementById("AvatarImage");
     image.src = `customers/${id}.png`;
-    id = id.split("_")[0];
 
     const htmlBody = document.body.innerHTML;
     const updatedHtmlBody = htmlBody
